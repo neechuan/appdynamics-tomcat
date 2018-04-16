c80007015e6d190cce3d8bdfc9262cceba8d3d6b
Gary Chew <neechuan@hotmail.com>
2018-04-16 22:10:23 +0800

    Delete push-to-dockerhub.sh

--- push-to-dockerhub.sh
+++
@@ -1,9 +0,0 @@
-# Login:
-export DOCKER_ID_USER=$1
-docker login
-
-# Tag:
-#docker tag appdynamics-tomcat $DOCKER_ID_USER/appdynamics-tomcat
-
-# Push:
-docker push $DOCKER_ID_USER/appdynamics-tomcat
